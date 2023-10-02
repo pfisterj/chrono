fd0caf18c18d48a61035b2081e98a4b8b9d5a338
Thomas Liang <lliang.thomas@gmail.com>
2023-10-02 15:00:36 -0500

    detecting where numpy is

--- contrib/packaging-python/conda/build.sh
+++ contrib/packaging-python/conda/build.sh
@@ -3,6 +3,7 @@ echo Started build.sh
 mkdir ./build
 cd ./build
 echo $CI_PROJECT_DIR
+export NP_INCL=$(python3 -c "import numpy; print(numpy.get_include())")
 echo $NP_INCL
 
 # Python libraries are different file types for MacOS and linux
